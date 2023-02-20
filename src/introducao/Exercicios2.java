cc18c9d21e89f3328dbecb4039993e621995d385
Guilherme de Aquino <49727289+GuilhermeAquinoDB@users.noreply.github.com>
2023-02-20 20:49:12 -0300

    exercicios 3 e 4

--- src/introducao/Exercicios2.java
+++ src/introducao/Exercicios2.java
@@ -6,11 +6,12 @@ public class Exercicios2 {
 
 	public static void main(String[] args) {
 		
-		int anos = 0, meses = 0, dias = 0, diasTotal = 0;
+		int anos = 0, meses = 0, dias = 0, diasTotal = 0, horas, minutos, segundos, segundosTotal, a, b, c;
+		double d, r, s;
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Digite a idade em anos: ");
+/*		System.out.print("Digite a idade em anos: ");
 		anos = sc.nextInt();
 		
 		System.out.print("Digite a idade em meses: ");
@@ -26,7 +27,7 @@ public class Exercicios2 {
         
   /*----------------------------------------------------------------------------*/
         
-        System.out.print("Digite sua idade expressa em dias : ");
+   /*     System.out.print("Digite sua idade expressa em dias : ");
         diasTotal = sc.nextInt();
         
         anos = diasTotal / 365;
@@ -36,8 +37,38 @@ public class Exercicios2 {
         //10824 **20/02/2023**     
         System.out.print("Você tem: " + anos + ", meses " + meses + ", e dias "+ dias);
         
- /*-------------------------------------------------------------------------------------------*/
+ 	/*-------------------------------------------------------------------------------------------*/
         
+		
+/*		System.out.print("Qual a duração do Evento expressa em segundos : ");
+		segundosTotal = sc.nextInt();
+		
+		
+		horas = segundosTotal / 3600;	
+		minutos = (segundosTotal % 3600) / 60;
+		segundos = segundosTotal % 60;
+		
+		System.out.println("Esse Evento terá a duração de: "+ horas + " horas, " + minutos + " minutos e " + segundos + " segundos.");
+		
+	/*---------------------------------------------------------------------------------------------------------------------------------*/
+		
+		System.out.print("Digite o primeiro numero inteiro e positivo: ");
+		a = sc.nextInt();
+		
+		System.out.print("Digite o segundo numero inteiro e positivo: ");
+		b = sc.nextInt();
+		
+		System.out.print("Digite o terceiro numero inteiro e positivo: ");
+		c = sc.nextInt();
+		
+		r = Math.sqrt(a + b);
+		
+		s = Math.sqrt(b + c);
+		
+		d = r + s / 2;
+		
+		System.out.printf("A seguinte expressão foi: %.2f", d);
+		
 		sc.close();
         
 	}
